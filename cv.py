b45512477b6f30503538fd7d3c9f1d29ff20afb8
Bhargav R. Raut <bhargav.r.raut@gmail.com>
2016-07-17 19:26:59 +0530

    finalized basic logic

--- cv.py
+++ cv.py
@@ -32,8 +32,6 @@ def erode_image( image ):
 	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))	
 	image = cv2.dilate(image,kernel,iterations = 1)
 	image =  cv2.GaussianBlur(image, (5, 5), 0)
-	#cv2.imshow("eroded",image)
-	#cv2.imwrite("eroded.jpg",image)
 	return image
 
 
@@ -51,16 +49,20 @@ def smooth ( image ):
 
 def find_relevant_contours ( image ):
 
-	##take each contour
-	##find how well it corresponds to an arc.
-	##find 
+	##get y coordinate of leftmost point of each contour, provided that it is not less than a certain predefined area, average these and this becomes the x-axis
+	##get x coordinate of the topmost point of each contour, same principle as above is to be applied., this becomes the y-axis.
+	##starting at the origin, find a contour whose highest and lowest points, or leftmost and rightmost points are on either side of a given axis.
+	##use three cardinal points from this contour to draw a circle.
+	##find out the areas of each of the four quadrants that are defined as a result of drawing the circle.
+	##repeat for as many contours as possible.
+	##the distortion should be able to predict the long axis.
 
 	return
 
 
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
-image = cv2.imread("sicst.jpg")
+image = cv2.imread("sics3.jpg")
 ratio = image.shape[0] / 500.0
 image = imutils.resize(image, height = 500)
 res_im = image.copy()
